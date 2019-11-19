4b4385bfac3fc48f624899ba28098c54dad04cf0
Yuriy Podporinov <benavir@mail.ru>
2019-11-19 12:49:00 +0300

    проверка через SOAP на удаление проекта

--- test/test_del_project.py
+++ test/test_del_project.py
@@ -7,10 +7,12 @@ def test_delete_some_project(app, check_ui):
     # assert app.session.is_logged_in_as("administrator")
     if len(app.project.get_project_list()) == 0:
         app.project.create_project(Project(name="delete"))
-    old_projects = app.project.get_project_list()
+    old_projects = app.soap.get_projects_list("administrator", "root")
+    # old_projects = app.project.get_project_list()
     project = random.choice(old_projects)
     app.project.delete_project(project)
-    new_projects = app.project.get_project_list()
+    new_projects = app.soap.get_projects_list("administrator", "root")
+    # new_projects = app.project.get_project_list()
     assert len(old_projects) - 1 == len(new_projects)
     old_projects.remove(project)
     assert old_projects == new_projects
